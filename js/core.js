cc3f00c773873a7cf454cb50884efe8c8e6a3c9b
Guergeiro <breno-salles@hotmail.com>
2018-11-28 16:04:01 +0000

    Started main page

--- js/core.js
+++ js/core.js
@@ -1,16 +1,25 @@
 var navButton = document.querySelector("#navButton");
 var fontawesome = document.querySelector("#fontawesome");
+var main = document.querySelector("main");
 
 navButton.addEventListener("click", function () {
 	if (fontawesome.classList.contains("fa-bars")) {
 		document.querySelector("nav").style.width = navButton.style.marginLeft = "200px";
-		document.querySelector("main").style.opacity = 0.1;
+		main.style.filter = "blur(50px)";
+		main.style.cursor = "default";
 		fontawesome.classList.remove("fa-bars");
 		fontawesome.classList.add("fa-times");
+		main.addEventListener("click", close);
 	} else {
-		document.querySelector("nav").style.width = navButton.style.marginLeft = "0";
-		document.querySelector("main").style.opacity = 1;
-		fontawesome.classList.remove("fa-times");
-		fontawesome.classList.add("fa-bars");
+		close();
 	}
 });
+
+function close() {
+	main.removeEventListener("click", close);
+	main.style.filter = "blur(0)";
+	main.style.cursor = "initial";
+	document.querySelector("nav").style.width = navButton.style.marginLeft = "0";
+	fontawesome.classList.remove("fa-times");
+	fontawesome.classList.add("fa-bars");
+}
