500e25928fb1643e01ba38abc45340a1ab5f9ff5
Breno Salles <breno-salles@hotmail.com>
2018-11-29 23:49:49 +0000

    Bunch of updates
    Improved js
    
    Fixed 'we are' div that would sometimes overlap in smaller heights screens and create a blank space below the footer

--- js/core.js
+++ js/core.js
@@ -1,51 +1,87 @@
-window.onscroll = topChecker;
+window.onscroll = toggleScrollButton;
 
 var navButton = document.querySelector("#navButton");
-var scrollButton = document.querySelector("#scrollButton");
+var navBar = document.querySelector("nav");
+
 var main = document.querySelector("main");
 var footer = document.querySelector("footer");
 
-/* Opens menu */
-document.querySelector("#navButton").addEventListener("click", function () {
-	if (!navButton.firstChild.classList.contains("close")) {
-		document.querySelector("nav").style.width = navButton.style.marginLeft = "200px";
-		main.style.filter = footer.style.filter = "blur(50px)";
-		main.style.cursor = footer.style.cursor = "default";
-		navButton.firstChild.classList.add("close");
-		main.addEventListener("click", close);
-		document.body.style.overflow = "hidden";
-		scrollButton.style.opacity = 0;
-	} else {
-		close();
-	}
-});
+/* Adds events listener to toggle menu */
+navButton.addEventListener("click", toggleMenu);
+main.addEventListener("click", closeMenu);
+footer.addEventListener("click", closeMenu);
 
-/* Closes menu */
-function close() {
-	main.removeEventListener("click", close);
+/* Toggle menu */
+function toggleMenu() {
+	if (!navButton.firstChild.classList.contains("close")) {
+		showMenu();
+	} else {
+		closeMenu();
+	}
+}
+
+/* Show menu */
+function showMenu() {
+	/* Shows navbar */
+	navBar.style.width = navButton.style.marginLeft = "200px";
+
+	/* Adds blur effect */
+	main.style.filter = footer.style.filter = "blur(50px)";
+	main.style.cursor = footer.style.cursor = "default";
+
+	/* Adds close class */
+	navButton.firstChild.classList.add("close");
+	document.body.style.overflow = "hidden";
+
+	/* Hide scrollButton */
+	hideScrollButton();
+}
+
+/* Close menu */
+function closeMenu() {
+	/* Hides navbar */
+	navBar.style.width = navButton.style.marginLeft = "0";
+
+	/* Removes blur effect */
 	main.style.filter = footer.style.filter = "blur(0)";
 	main.style.cursor = footer.style.cursor = "initial";
-	document.querySelector("nav").style.width = navButton.style.marginLeft = "0";
+
+	/* Removes close class */
 	navButton.firstChild.classList.remove("close");
 	document.body.style.overflow = "initial";
-	topChecker();
+
+	/* Tries to show scrollButton */
+	toggleScrollButton();
 }
 
-/* Shows/Hides scrollButton */
-function topChecker() {
-	if (document.body.scrollTop > window.innerHeight / 2 || document.documentElement.scrollTop > window.innerHeight / 2) {
-		scrollButton.style.opacity = 1;
-		scrollButton.style.cursor = "pointer";
-		scrollButton.addEventListener("click", scrollTop);
-	} else {
-		scrollButton.style.opacity = 0;
-		scrollButton.style.cursor = "default";
-		scrollButton.removeEventListener("click", scrollTop);
-	}
-}
+
+var scrollButton = document.querySelector("#scrollButton");
+
+scrollButton.addEventListener("click", scrollTop);
 
 /* Scrolls page */
 function scrollTop() {
 	document.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
+}
+
+/* Check current page position and shows button if necessary */
+function toggleScrollButton() {
+	if (document.body.scrollTop > window.innerHeight / 2 || document.documentElement.scrollTop > window.innerHeight / 2) {
+		showScrollButton();
+	} else {
+		hideScrollButton();
+	}
+}
+
+/* Show scrollButton */
+function showScrollButton() {
+	scrollButton.style.opacity = 1;
+	scrollButton.style.pointerEvents = "initial";
+}
+
+/* Hide scrollButton */
+function hideScrollButton() {
+	scrollButton.style.opacity = 0;
+	scrollButton.style.pointerEvents = "none";
 }
