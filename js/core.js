68c45cc3c943c691b203aaffe4f4b2687afdc6e5
Guergeiro <breno-salles@hotmail.com>
2018-11-29 15:40:16 +0000

    Improved navbar and added scroll button

--- js/core.js
+++ js/core.js
@@ -1,14 +1,12 @@
 var navButton = document.querySelector("#navButton");
-var fontawesome = document.querySelector("#fontawesome");
 var main = document.querySelector("main");
 
-navButton.addEventListener("click", function () {
-	if (fontawesome.classList.contains("fa-bars")) {
+document.querySelector("#navButton").addEventListener("click", function () {
+	if (!navButton.firstChild.classList.contains("close")) {
 		document.querySelector("nav").style.width = navButton.style.marginLeft = "200px";
 		main.style.filter = "blur(50px)";
 		main.style.cursor = "default";
-		fontawesome.classList.remove("fa-bars");
-		fontawesome.classList.add("fa-times");
+		navButton.firstChild.classList.add("close");
 		main.addEventListener("click", close);
 	} else {
 		close();
@@ -20,6 +18,20 @@ function close() {
 	main.style.filter = "blur(0)";
 	main.style.cursor = "initial";
 	document.querySelector("nav").style.width = navButton.style.marginLeft = "0";
-	fontawesome.classList.remove("fa-times");
-	fontawesome.classList.add("fa-bars");
+	navButton.firstChild.classList.remove("close");
 }
+
+var scrollButton = document.querySelector("#scrollButton");
+
+window.onscroll = function () {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		scrollButton.style.opacity = 1;
+	} else {
+		scrollButton.style.opacity = 0;
+	}
+}
+
+scrollButton.addEventListener("click", function () {
+	document.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+});
