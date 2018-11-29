639ebb93d85a8c37f53e353d4d3c26e33a6cd45b
Guergeiro <breno-salles@hotmail.com>
2018-11-29 17:03:13 +0000

    Added footer and improved js

--- js/core.js
+++ js/core.js
@@ -1,6 +1,10 @@
+window.onscroll = topChecker;
+
 var navButton = document.querySelector("#navButton");
+var scrollButton = document.querySelector("#scrollButton");
 var main = document.querySelector("main");
 
+/* Opens menu */
 document.querySelector("#navButton").addEventListener("click", function () {
 	if (!navButton.firstChild.classList.contains("close")) {
 		document.querySelector("nav").style.width = navButton.style.marginLeft = "200px";
@@ -8,30 +12,39 @@ document.querySelector("#navButton").addEventListener("click", function () {
 		main.style.cursor = "default";
 		navButton.firstChild.classList.add("close");
 		main.addEventListener("click", close);
+		document.body.style.overflow = "hidden";
+		scrollButton.style.opacity = 0;
 	} else {
 		close();
 	}
 });
 
+/* Closes menu */
 function close() {
 	main.removeEventListener("click", close);
 	main.style.filter = "blur(0)";
 	main.style.cursor = "initial";
 	document.querySelector("nav").style.width = navButton.style.marginLeft = "0";
 	navButton.firstChild.classList.remove("close");
+	document.body.style.overflow = "initial";
+	topChecker();
 }
 
-var scrollButton = document.querySelector("#scrollButton");
-
-window.onscroll = function () {
-	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+/* Shows/Hides scrollButton */
+function topChecker() {
+	if (document.body.scrollTop > window.innerHeight / 2 || document.documentElement.scrollTop > window.innerHeight / 2) {
 		scrollButton.style.opacity = 1;
+		scrollButton.style.cursor = "pointer";
+		scrollButton.addEventListener("click", scrollTop);
 	} else {
 		scrollButton.style.opacity = 0;
+		scrollButton.style.cursor = "default";
+		scrollButton.removeEventListener("click", scrollTop);
 	}
 }
 
-scrollButton.addEventListener("click", function () {
+/* Scrolls page */
+function scrollTop() {
 	document.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
-});
+}
